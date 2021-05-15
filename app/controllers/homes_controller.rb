40bcb7ea69b9814988a8ceaa2d13c7605f8a5311
S-Toky <stokyr29@gmail.com>
2021-05-15 10:31:47 +0300

    add modify

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,6 @@
 class HomesController < ApplicationController
   def index
-    @boards = Board.all.order(created_at: :desc)
-    @projects = Project.all.order(created_at: :desc)
+    @boards = current_user.boards.order(created_at: :desc)
+    @projects = current_user.projects.order(created_at: :desc)
   end
 end
